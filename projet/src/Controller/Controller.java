6c55db4492b8e3d9134483e2c91756d6fb60f41a
cravier <clementra.ravier@gmail.com>
2017-10-06 18:48:39 +0200

    Test

--- projet/src/Controller/Controller.java
+++ projet/src/Controller/Controller.java
@@ -30,17 +30,52 @@ public class Controller {
 
 
         try {
+
+            /**
+             * On reçoit les premières infos (nom du fichier etc)
+             */
             ds.receive(dp);
 
             String rcvd = new String(dp.getData(), 0, dp.getLength()) + ", from address: "
                     + dp.getAddress() + ", port: " + dp.getPort();
             System.out.println(rcvd);
 
+            /**
+             * On créé un fichier qui nous servira de fichier d'accueil
+             */
 
-            File file = new File(new String(dp.getData()));
-            FileInputStream fis = null;
+            File file = new File("C:\\Users\\Epulapp\\Documents\\_Projets\\Arsir\\projet\\Ressources\\outputfile.txt");
+            file.createNewFile();
+            FileOutputStream fis = new FileOutputStream(file);
 
-            fis = new FileInputStream(file);
+            /**
+             * Acknoledgement à pumpkin
+             */
+
+            byte[] b = ("Acknowledged").getBytes();
+            InetAddress ha = InetAddress.getByName("127.0.0.1");
+            DatagramSocket ds1 = new DatagramSocket(dp.getPort(), ha);
+            System.out.println("Sending back acknowledge to client");
+            ds1.send(new DatagramPacket(b,b.length,dp.getAddress(),dp.getPort()));
+
+
+            /**
+             * index pour savoir combien de parties du fichier on récupère
+             */
+            int index=0;
+            while (index != 2){
+                System.out.println("Récupération partie "+index + " du fichier.");
+                byte[] buffer = new byte[512];
+                DatagramPacket dp1 = new DatagramPacket(buffer, buffer.length);
+
+                ds1.receive(dp1);
+
+                String receive = new String(dp1.getData(), 0, dp1.getLength()) + ", from address: "
+                        + dp1.getAddress() + ", port: " + dp1.getPort();
+                System.out.println(receive);
+
+                index++;
+            }
 
             /*System.out.println("Total file size to read (in bytes) : "
                     + fis.available());
